d9202cc97c9cee14d9e9ac9bf36b93f2c2eed1ca
Brano Kemen <cameni@gmail.com>
2016-08-13 11:10:09 +0200

    otbullet changed to VS2015, moved solution

--- src/copy-headers.bat
+++ src/copy-headers.bat
@@ -7,11 +7,11 @@ xcopy LinearMath ..\..\..\include\bullet\LinearMath\ /sy /exclude:copy-headers.e
 xcopy *.h ..\..\..\include\bullet\ /y
 xcopy otbullet ..\..\..\include\bullet\otbullet\ /sy /exclude:copy-headers.exc
 
-xcopy ..\bin\Debug ..\..\..\lib.12\Win32\Debug\ /sy /exclude:copy-headers.exc
-xcopy ..\bin\Release ..\..\..\lib.12\Win32\Release\ /sy /exclude:copy-headers.exc
-xcopy ..\bin\ReleaseLTCG ..\..\..\lib.12\Win32\ReleaseLTCG\ /sy /exclude:copy-headers.exc
+rem xcopy ..\bin\Win32\Debug ..\..\..\lib.14\Win32\Debug\ /sy /exclude:copy-headers.exc
+rem xcopy ..\bin\Win32\Release ..\..\..\lib.14\Win32\Release\ /sy /exclude:copy-headers.exc
+rem xcopy ..\bin\Win32\ReleaseLTCG ..\..\..\lib.14\Win32\ReleaseLTCG\ /sy /exclude:copy-headers.exc
 
-xcopy ..\bin\ReleaseLTCG\otbullet.dll ..\..\..\..\bin\ /y
-xcopy ..\bin\ReleaseLTCG\otbullet.pdb ..\..\..\..\bin\ /y
-xcopy ..\bin\Debug\otbulletd.dll ..\..\..\..\bin\ /y
-xcopy ..\bin\Debug\otbulletd.pdb ..\..\..\..\bin\ /y
+xcopy ..\bin\Win32\ReleaseLTCG\otbullet.dll ..\..\..\..\bin\ /y
+xcopy ..\bin\Win32\ReleaseLTCG\otbullet.pdb ..\..\..\..\bin\ /y
+xcopy ..\bin\Win32\Debug\otbulletd.dll ..\..\..\..\bin\ /y
+xcopy ..\bin\Win32\Debug\otbulletd.pdb ..\..\..\..\bin\ /y
