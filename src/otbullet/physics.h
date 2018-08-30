e7a5ff97ca736516195fa7bba55de085b83803e4
Cyril <cyril.gramblicka@gmail.com>
2018-08-30 09:39:49 +0200

    mesh id added to LocalRayResult

--- src/otbullet/physics.h
+++ src/otbullet/physics.h
@@ -170,7 +170,14 @@ public:
         if (_cleaner) _cleaner(this,0);
     }
 
-    static const int HASHID = 2650151887;
+    ///Interface revision hash
+    static const int HASHID = 2633374268;
+    
+    ///Interface name (full ns::class string)
+    static const coid::tokenhash& IFCNAME() {
+        static const coid::tokenhash _name = "bt::physics";
+        return _name;
+    }
 
     int intergen_hash_id() const override final { return HASHID; }
 
@@ -179,19 +186,20 @@ public:
     }
 
     const coid::tokenhash& intergen_interface_name() const override final {
-        static const coid::tokenhash _name = "bt::physics";
-        return _name;
+        return IFCNAME();
     }
 
     static const coid::token& intergen_default_creator_static( EBackend bck ) {
-        static const coid::token _dc("bt::physics.get@2650151887");
+        static const coid::token _dc("bt::physics.get@2633374268");
         static const coid::token _djs("bt::physics@wrapper.js");
+        static const coid::token _djsc("bt::physics@wrapper.jsc");
         static const coid::token _dlua("bt::physics@wrapper.lua");
         static const coid::token _dnone;
 
         switch(bck) {
         case IFC_BACKEND_CXX: return _dc;
         case IFC_BACKEND_JS:  return _djs;
+        case IFC_BACKEND_JSC:  return _djsc;
         case IFC_BACKEND_LUA: return _dlua;
         default: return _dnone;
         }
@@ -211,6 +219,7 @@ public:
     void* intergen_wrapper( EBackend bck ) const override final {
         switch(bck) {
         case IFC_BACKEND_JS: return intergen_wrapper_cache<IFC_BACKEND_JS>();
+        case IFC_BACKEND_JSC: return intergen_wrapper_cache<IFC_BACKEND_JSC>();
         case IFC_BACKEND_LUA: return intergen_wrapper_cache<IFC_BACKEND_LUA>();
         default: return 0;
         }
@@ -236,8 +245,8 @@ public:
         type.consume("struct ");
 
         coid::charstr tmp = "bt::physics";
-        tmp << "@client" << '.' << type;
-        
+        tmp << "@client-2633374268" << '.' << type;
+
         coid::interface_register::register_interface_creator(tmp, cc);
         return 0;
     }
@@ -258,17 +267,20 @@ inline iref<T> physics::create( T* _subclass_, double r, void* context )
     typedef iref<T> (*fn_creator)(physics*, double, void*);
 
     static fn_creator create = 0;
-    static const coid::token ifckey = "bt::physics.create@2650151887";
+    static const coid::token ifckey = "bt::physics.create@2633374268";
 
     if (!create)
         create = reinterpret_cast<fn_creator>(
             coid::interface_register::get_interface_creator(ifckey));
 
-    if (!create)
-        throw coid::exception("interface creator inaccessible: ") << ifckey;
+    if (!create) {
+        log_mismatch("create", "bt::physics.create", "@2633374268");
+        return 0;
+    }
 
     return create(_subclass_, r, context);
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 template<class T>
 inline iref<T> physics::get( T* _subclass_ )
@@ -276,14 +288,16 @@ inline iref<T> physics::get( T* _subclass_ )
     typedef iref<T> (*fn_creator)(physics*);
 
     static fn_creator create = 0;
-    static const coid::token ifckey = "bt::physics.get@2650151887";
+    static const coid::token ifckey = "bt::physics.get@2633374268";
 
     if (!create)
         create = reinterpret_cast<fn_creator>(
             coid::interface_register::get_interface_creator(ifckey));
 
-    if (!create)
-        throw coid::exception("interface creator inaccessible: ") << ifckey;
+    if (!create) {
+        log_mismatch("get", "bt::physics.get", "@2633374268");
+        return 0;
+    }
 
     return create(_subclass_);
 }
---
+++ src/otbullet/shape_info_cfg.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef __OTBULLET_SHAPE_INFO_CFG_H__
+#define __OTBULLET_SHAPE_INFO_CFG_H__
+
+struct shape_info_base {
+    const unsigned short _mesh_id;
+    shape_info_base(unsigned short mesh_id)
+        : _mesh_id(mesh_id)
+    {}
+};
+
+#endif // __OTBULLET_SHAPE_INFO_CFG_H__
